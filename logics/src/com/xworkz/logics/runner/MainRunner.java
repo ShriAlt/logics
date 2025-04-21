d266fbc67e70129ec44c54915380311699489244
Shriharsha K M <shriharshakm10@gmail.com>
2025-04-21 17:52:28 +0530

    updating prime number

--- logics/src/com/xworkz/logics/runner/MainRunner.java
+++ logics/src/com/xworkz/logics/runner/MainRunner.java
@@ -5,7 +5,7 @@ import com.xworkz.logics.internal.PrimeNumber;
 
 public class MainRunner {
     public static void main(String[] args) {
-        ArmstrongNumber armstrongNumber=new ArmstrongNumber();
+        ArmstrongNumber armstrongNumber=new ArmstrongNumber();;
      armstrongNumber.isNumberIsArmstrong(3);
         System.out.printf("number is armstrong :"+armstrongNumber.isNumberIsArmstrong(153));
         PrimeNumber primeNumber=new PrimeNumber();
